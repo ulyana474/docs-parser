da7aa2326b7522c04446f4428b197a0f2a2e75c8
Ulyana.Drozdova <ulyana.drozdova@innowise-group.com>
2023-03-27 14:06:41 -0700

    added alembic migrations on postgres in docker

---
+++ auth/app/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    role = Column(String)
+    
+    class Config:
+        orm_mode = True
