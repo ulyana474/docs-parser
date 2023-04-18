8287dbc9100afd10b0cb18ae02c5d8c2603b653f
Ulyana.Drozdova <ulyana.drozdova@innowise-group.com>
2023-04-18 15:30:53 -0700

    endpoint for getting docs by id

--- core/app/repository/articles.py
+++ core/app/repository/articles.py
@@ -1,36 +1,57 @@
-from fastapi import status, Depends, Response
 from typing import List
-from .base import BaseRepository
-from core.app.utils.parse import parse_articles
-from core.app.database.mongo import get_database
-from motor.motor_asyncio import AsyncIOMotorClient
-from core.app.models.articles import Article
+
+from bson.objectid import ObjectId
+from fastapi import Body, Depends, Response, status, HTTPException
 from fastapi.encoders import jsonable_encoder
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from core.app.database.mongo import get_database
+from core.app.schemas.articles import Article
+from core.app.utils.parse import parse_articles
+
+from .base import BaseRepository
 
 
 class Repository(BaseRepository):
 
     @staticmethod
-    async def get(db: AsyncIOMotorClient = Depends(get_database)) -> List:
+    def get_collection(db: AsyncIOMotorClient = Depends(get_database)):
         collection = db.get_collection('docs')
+        return collection
+
+    @staticmethod
+    async def get(db: AsyncIOMotorClient = Depends(get_database)) -> List:
+        collection = Repository.get_collection(db)
         cursor = collection.find()
         docs_to_show = []
         async for document in cursor:
-            document["_id"] = str(document["_id"])
+            document["_id"] = str(document["_id"])  # ObjectId for Mongo
             docs_to_show.append(document)
         return docs_to_show
 
     @staticmethod
+    async def get_by_id(docs_id: str, db: AsyncIOMotorClient = Depends(get_database)):
+        collection = Repository.get_collection(db)
+        if (existing_docs := await collection.find_one({'_id': ObjectId(docs_id)})) is not None:
+            return existing_docs
+        else:
+            raise HTTPException(status_code=404, content="not found")  
+
+    @staticmethod
     async def create(db: AsyncIOMotorClient = Depends(get_database)):
-        collection = db.get_collection('docs')
+        collection = Repository.get_collection(db)
         new_docs = parse_articles()
         for doc in new_docs:
             await collection.insert_one(jsonable_encoder(doc))
         return new_docs
 
     @staticmethod
-    async def update(db: AsyncIOMotorClient = Depends(get_database)):
-        pass
+    async def update(docs_id: str, docs: Article = Body(...), db: AsyncIOMotorClient = Depends(get_database)):
+        collection = Repository.get_collection(db)
+        docs = {k: v for k, v in docs.dict().items() if v is not None}
+        print(docs)
+        update_result = await collection.update_one({"_id": docs_id}, {"$set": docs})
+        return update_result
 
     @staticmethod
     async def delete(db: AsyncIOMotorClient = Depends(get_database)):
@@ -38,6 +59,6 @@ class Repository(BaseRepository):
 
     @staticmethod
     async def delete_all(db: AsyncIOMotorClient = Depends(get_database)):
-        collection = db.get_collection('docs')
+        collection = Repository.get_collection(db)
         result = await collection.delete_many({})
         return result.deleted_count
--- core/app/routers/api/v1/articles.py
+++ core/app/routers/api/v1/articles.py
@@ -5,7 +5,7 @@ from fastapi.responses import JSONResponse
 from core.app.dependencies.auth import has_access
 from core.app.utils.parse import parse_articles
 from core.app.repository.articles import Repository
-from core.app.models.articles import Article
+from core.app.schemas.articles import Article
 from core.app.repository.articles import Repository
 
 
@@ -29,9 +29,19 @@ async def get_docs(docs=Depends(Repository.get)):
     return {'docs': docs}
 
 
+@router.get("/get-docs-by-id/{docs_id}", status_code=status.HTTP_200_OK, response_model=Article)
+async def get_docs_by_id(docs_id: str, docs=Depends(Repository.get_by_id)):
+    return docs
+
+
 @router.post("/insert-docs", status_code=status.HTTP_201_CREATED)
 async def insert_docs(res=Depends(Repository.create)):
     return {'inserted': res}
+
+
+@router.put("/update-docs/{docs_id}", status_code=status.HTTP_200_OK, response_model=Article)
+async def update_docs(docs=Depends(Repository.update)):
+    return {'updated': docs}
 
 
 @router.delete("/delete-all-docs", status_code=status.HTTP_204_NO_CONTENT)
